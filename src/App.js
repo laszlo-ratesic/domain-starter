7f86262b60e2f85b7cf0f099c61cdf34de888f66
laszlo-ratesic <88006245+laszlo-ratesic@users.noreply.github.com>
2022-02-19 02:57:09 -0600

    update react app

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './styles/App.css';
 import twitterLogo from './assets/twitter-logo.svg';
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
+const TWITTER_HANDLE = 'laszlo-ratesic';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
@@ -15,8 +15,8 @@ const App = () => {
 				<div className="header-container">
 					<header>
             <div className="left">
-              <p className="title">🐱‍👤 Ninja Name Service</p>
-              <p className="subtitle">Your immortal API on the blockchain!</p>
+              <p className="title">🐱‍🐉 GangGang Name Service</p>
+              <p className="subtitle">"It's that gang-gang type shi"</p>
             </div>
 					</header>
 				</div>
@@ -28,7 +28,7 @@ const App = () => {
 						href={TWITTER_LINK}
 						target="_blank"
 						rel="noreferrer"
-					>{`built with @${TWITTER_HANDLE}`}</a>
+					>{`built by @${TWITTER_HANDLE}`}</a>
 				</div>
 			</div>
 		</div>
